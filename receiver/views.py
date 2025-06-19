0c9ff1504ab6dd815bff47718e0e5823addd4792
loke <lokendar384@gmail.com>
2025-06-19 14:37:45 +0545

    new reports app create

--- receiver/views.py
+++ receiver/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import render
 class InquiryViewSet(viewsets.ModelViewSet):
     queryset = Inquiry.objects.all()
     serializer_class = InquirySerializer
-    permission_classes = [AllowAny]  # Restrict in production
-    http_method_names = ['get', 'post']  # Only allow GET and POST
+    permission_classes = [AllowAny]        # Restrict in production
+    http_method_names = ['get', 'post']    # Only allow GET and POST
 
 def receiver_list(request):
     inquiries = Inquiry.objects.all()
