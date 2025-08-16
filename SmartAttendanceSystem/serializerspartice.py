22edc624536a0db6db5b211f6886399e1a0a395f
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-08-16 12:11:26 +0545

    .

--- SmartAttendanceSystem/serializerspartice.py
+++ SmartAttendanceSystem/serializerspartice.py
@@ -537,3 +537,277 @@ class APIResponseSerializer(serializers.Serializer):
     data = serializers.JSONField(required=False)
     errors = serializers.DictField(required=False)
     meta = serializers.DictField(required=False)
+    
+    
+    
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+import base64
+from django.core.files.base import ContentFile
+from .models import (
+    User, School, Department, Class, Student, Teacher, Subject,
+    FaceEncoding, AttendanceSession, AttendanceRecord, AttendanceAlert,
+    AttendanceReport, SystemConfiguration, AuditLog
+)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Base user serializer"""
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name', 'role',
+            'phone_number', 'profile_image', 'date_of_birth', 'address',
+            'emergency_contact', 'is_active_student', 'password', 'confirm_password'
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'id': {'read_only': True}
+        }
+
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('confirm_password'):
+            raise serializers.ValidationError("Passwords don't match")
+        attrs.pop('confirm_password', None)
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    """Login serializer"""
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError('Invalid credentials')
+            if not user.is_active:
+                raise serializers.ValidationError('User account is disabled')
+            attrs['user'] = user
+            return attrs
+        else:
+            raise serializers.ValidationError('Must include username and password')
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    """School serializer"""
+    
+    class Meta:
+        model = School
+        fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    """Department serializer"""
+    head_of_department_name = serializers.CharField(source='head_of_department.get_full_name', read_only=True)
+    school_name = serializers.CharField(source='school.name', read_only=True)
+    
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class ClassSerializer(serializers.ModelSerializer):
+    """Class serializer"""
+    department_name = serializers.CharField(source='department.name', read_only=True)
+    class_teacher_name = serializers.CharField(source='class_teacher.get_full_name', read_only=True)
+    student_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Class
+        fields = '__all__'
+    
+    def get_student_count(self, obj):
+        return obj.students.filter(user__is_active_student=True).count()
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    """Subject serializer"""
+    department_name = serializers.CharField(source='department.name', read_only=True)
+    teacher_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Subject
+        fields = '__all__'
+    
+    def get_teacher_count(self, obj):
+        return obj.teachers.count()
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    """Student serializer"""
+    user = UserSerializer()
+    class_name = serializers.CharField(source='class_assigned.name', read_only=True)
+    parent_name = serializers.CharField(source='parent_guardian.get_full_name', read_only=True)
+    attendance_percentage = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Student
+        fields = '__all__'
+    
+    def get_attendance_percentage(self, obj):
+        # Calculate attendance percentage for current month
+        from django.utils import timezone
+        from datetime import datetime
+        from django.db.models import Count
+        
+        current_month = timezone.now().replace(day=1)
+        total_sessions = AttendanceRecord.objects.filter(
+            student=obj,
+            session__session_date__gte=current_month
+        ).count()
+        
+        if total_sessions == 0:
+            return 100.0
+            
+        present_sessions = AttendanceRecord.objects.filter(
+            student=obj,
+            session__session_date__gte=current_month,
+            status__in=['present', 'late']
+        ).count()
+        
+        return round((present_sessions / total_sessions) * 100, 2)
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_data['role'] = 'student'
+        user = UserSerializer().create(user_data)
+        student = Student.objects.create(user=user, **validated_data)
+        return student
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', None)
+        if user_data:
+            user_serializer = UserSerializer(instance.user, data=user_data, partial=True)
+            user_serializer.is_valid(raise_exception=True)
+            user_serializer.save()
+        
+        return super().update(instance, validated_data)
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    """Teacher serializer"""
+    user = UserSerializer()
+    department_name = serializers.CharField(source='department.name', read_only=True)
+    subject_names = serializers.StringRelatedField(source='subjects', many=True, read_only=True)
+    classes_teaching = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+    
+    def get_classes_teaching(self, obj):
+        return [cls.name for cls in obj.teaching_classes.all()]
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        subjects_data = validated_data.pop('subjects', [])
+        user_data['role'] = 'teacher'
+        user = UserSerializer().create(user_data)
+        teacher = Teacher.objects.create(user=user, **validated_data)
+        teacher.subjects.set(subjects_data)
+        return teacher
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', None)
+        subjects_data = validated_data.pop('subjects', None)
+        
+        if user_data:
+            user_serializer = UserSerializer(instance.user, data=user_data, partial=True)
+            user_serializer.is_valid(raise_exception=True)
+            user_serializer.save()
+        
+        if subjects_data is not None:
+            instance.subjects.set(subjects_data)
+        
+        return super().update(instance, validated_data)
+
+
+class FaceEncodingSerializer(serializers.ModelSerializer):
+    """Face encoding serializer"""
+    student_name = serializers.CharField(source='student.user.get_full_name', read_only=True)
+    student_id = serializers.CharField(source='student.student_id', read_only=True)
+    algorithm_display = serializers.CharField(source='get_algorithm_display', read_only=True)
+    
+    class Meta:
+        model = FaceEncoding
+        fields = '__all__'
+        extra_kwargs = {
+            'encoding_data': {'write_only': True}
+        }
+
+
+class FaceEncodingCreateSerializer(serializers.ModelSerializer):
+    """Face encoding creation with image upload"""
+    face_image_base64 = serializers.CharField(write_only=True, required=False)
+    
+    class Meta:
+        model = FaceEncoding
+        fields = [
+            'student', 'algorithm', 'confidence_threshold', 
+            'face_image_base64', 'encoding_data'
+        ]
+    
+    def create(self, validated_data):
+        face_image_base64 = validated_data.pop('face_image_base64', None)
+        
+        if face_image_base64:
+            # Decode base64 image and save
+            format, imgstr = face_image_base64.split(';base64,')
+            ext = format.split('/')[-1]
+            image = ContentFile(
+                base64.b64decode(imgstr),
+                name=f"face_{validated_data['student'].student_id}_{validated_data['algorithm']}.{ext}"
+            )
+            # Save image path
+            validated_data['face_image_path'] = f"face_images/{image.name}"
+        
+        return super().create(validated_data)
+
+
+class AttendanceSessionSerializer(serializers.ModelSerializer):
+    """Attendance session serializer"""
+    class_name = serializers.CharField(source='class_assigned.name', read_only=True)
+    subject_name = serializers.CharField(source='subject.name', read_only=True)
+    teacher_name = serializers.CharField(source='teacher.user.get_full_name', read_only=True)
+    attendance_percentage = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = AttendanceSession
+        fields = '__all__'
+    
+    def get_attendance_percentage(self, obj):
+        if obj.total_students == 0:
+            return 0.0
+        return round((obj.present_count / obj.total_students) * 100, 2)
+
+
+class AttendanceRecordSerializer(serializers.ModelSerializer):
+    """Attendance record serializer"""
+    student_name = serializers.CharField(source='student.user.get_full_name', read_only=True)
+    student_id = serializers.CharField(source='student.student_id', read_only=True)
+    class_name = serializers.CharField(source='session.class_assigned.name', read_only=True)
+    subject_name = serializers.CharField(source='session.subject.name', read_only=True)
+    session_date = serializers.DateField(source='session.session_date', read_only=True)
+    
+    class Meta:
+        model = AttendanceRecord
+        fields = '__all__'
+
+
